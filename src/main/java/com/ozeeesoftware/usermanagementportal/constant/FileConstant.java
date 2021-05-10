5bd679b857aa485c41307dfef5f5971c3f22ce73
ozankocali <ozan.kocali@hotmail.com>
2021-05-10 13:59:33 +0300

    Minor fix

--- src/main/java/com/ozeeesoftware/usermanagementportal/constant/FileConstant.java
+++ src/main/java/com/ozeeesoftware/usermanagementportal/constant/FileConstant.java
@@ -5,7 +5,7 @@ public class FileConstant {
     public static final String JPG_EXTENSION = "jpg";
     public static final String USER_FOLDER = System.getProperty("user.home") + "/usermanagementportal/user/";
     public static final String DIRECTORY_CREATED = "Created directory for: ";
-    public static final String DEFAULT_USER_IMAGE_PATH = "/user/image/profile/";
+    public static final String DEFAULT_USER_IMAGE_PATH = "api/v1/user/image/profile/";
     public static final String FILE_SAVED_IN_FILE_SYSTEM = "Saved file in file system by name: ";
     public static final String DOT = ".";
     public static final String FORWARD_SLASH = "/";
